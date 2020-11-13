ffd8ad46bb54d0a306b2206d2058b020cfd7e2de
06953 <06953@farnborough.ac.uk>
2020-11-13 15:28:22 +0000

    fffff

---
+++ src/test/java/Main.java
@@ -0,0 +1,34 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String myString = "Samsung exploding galaxies";
+        System.out.println(myString.toUpperCase());
+        System.out.print(myString.substring(5, 10));
+        System.out.println();
+
+        for (int i = 9; i < myString.length(); i++) {
+            System.out.print(myString.charAt(i));
+        }
+        System.out.println();
+        if (Objects.equals(myString, "Samsung")) {
+            System.out.println("Strings are the same");
+        } else {
+            System.out.println("Strings are not the same");
+        }
+
+        String text1 = "HELLO";
+        String text2 = "Hello";
+        System.out.println(true);
+        System.out.println(text2.charAt(0));
+        System.out.println((int) text2.charAt(0));
+        System.out.println(text2.indexOf('e'));
+        System.out.println(text2.lastIndexOf('l'));
+
+        int n = Integer.parseInt("22");
+        System.out.println(n + n);
+    }
+}
+
